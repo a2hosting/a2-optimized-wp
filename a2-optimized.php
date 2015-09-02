264129b00a08086bc4147b410f20a515a9ad1715
Benjamin Cool <bcool@a2hosting.com>
2015-09-02 10:56:40 -0400

    Added A2_Plugin_Dir global

--- a2-optimized.php
+++ a2-optimized.php
@@ -19,8 +19,11 @@
 class A2_Optimized {
   function __construct() {
 
+      $GLOBALS['A2_Plugin_Dir'] = dirname(__FILE__);
+
       require_once('A2_Optimized_Plugin.php');
       require_once ABSPATH.'wp-admin/includes/plugin.php';
+
 
       $a2Plugin = new A2_Optimized_Plugin();
 
