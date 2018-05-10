ef5e2ac1e6ce80aed00ede2783f0293334003bab
Peter Shackelford <pshackelford@a2hosting.com>
2018-05-10 14:37:04 -0400

    updated version numbers

--- a2-optimized.php
+++ a2-optimized.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: A2 Optimized WP
 	Plugin URI: https://wordpress.org/plugins/a2-optimized/
-	Version: 2.0.10.7.2
+	Version: 2.0.10.7.3
 	Author: A2 Hosting
 	Author URI: https://www.a2hosting.com/
 	Description: A2 Optimized - WordPress Optimization Plugin
@@ -23,7 +23,7 @@ class A2_Optimized {
 		$A2_Optimized_minimalRequiredPhpVersion = '5.3.0';
 
 		if (version_compare(PHP_VERSION, $A2_Optimized_minimalRequiredPhpVersion) < 0) {
-			add_action('admin_notices', array(&$this,'A2_Optimized_noticePhpVersionWrong'));
+			add_action('admin_notices', [&$this,'A2_Optimized_noticePhpVersionWrong']);
 
 			return;
 		}
@@ -34,7 +34,7 @@ class A2_Optimized {
 
 	  // Install the plugin
 	  if (!$a2Plugin->isInstalled()) {
-		$a2Plugin->install();
+	  	$a2Plugin->install();
 	  } else {
 	  	// Perform any version-upgrade activities prior to activation (e.g. database changes)
 		$a2Plugin->upgrade();
@@ -44,10 +44,10 @@ class A2_Optimized {
 	  $a2Plugin->addActionsAndFilters();
 
 	  // Register the Plugin Activation Hook
-	  register_activation_hook(__FILE__, array(&$a2Plugin, 'activate'));
+	  register_activation_hook(__FILE__, [&$a2Plugin, 'activate']);
 
 	  // Register the Plugin Deactivation Hook
-	  register_deactivation_hook(__FILE__, array(&$a2Plugin, 'deactivate'));
+	  register_deactivation_hook(__FILE__, [&$a2Plugin, 'deactivate']);
 	}
 
 	public function A2_Optimized_noticePhpVersionWrong() {
@@ -61,32 +61,32 @@ class A2_Optimized {
 	}
 
 	// add plugin upgrade notification
-	function showUpgradeNotification($currentPluginMetadata){
+	public function showUpgradeNotification($currentPluginMetadata) {
 		// Notice Transient
 		$upgrade_notices = get_transient('a2_opt_ug_notes');
-		if(!$upgrade_notices){
+		if (!$upgrade_notices) {
 			$response = wp_remote_get( 'http://wp-plugins.a2hosting.com/wp-json/wp/v2/update_notice?notice_plugin=2' );
 			if ( is_array( $response ) ) {
-				$upgrade_notices = array();
+				$upgrade_notices = [];
 				$body = json_decode($response['body']); // use the content
-				foreach($body as $item){
-					$upgrade_notices[$item->title->rendered] = "Version " . $item->title->rendered . ": " . strip_tags($item->content->rendered);
-				};
+				foreach ($body as $item) {
+					$upgrade_notices[$item->title->rendered] = 'Version ' . $item->title->rendered . ': ' . strip_tags($item->content->rendered);
+				}
 				set_transient('a2_opt_ug_notes', $upgrade_notices, 3600 * 12);
 			} else {
 				return;
-			};
-		};
+			}
+		}
 
-		foreach($upgrade_notices as $ver => $notice){
-			if (version_compare($currentPluginMetadata['Version'], $ver) < 0){
-	        	echo '</div><p style="background-color: #d54e21; padding: 10px; color: #f9f9f9; margin-top: 10px" class="update-message notice inline notice-warning notice-alt"><strong>Important Upgrade Notice:</strong><br />';
-	        	echo esc_html($notice), '</p><div>';
-	        	break;
+		foreach ($upgrade_notices as $ver => $notice) {
+			if (version_compare($currentPluginMetadata['Version'], $ver) < 0) {
+				echo '</div><p style="background-color: #d54e21; padding: 10px; color: #f9f9f9; margin-top: 10px" class="update-message notice inline notice-warning notice-alt"><strong>Important Upgrade Notice:</strong><br />';
+				echo esc_html($notice), '</p><div>';
+				break;
 			}
 		}
 	}
 }
 
 $a2opt_class = new A2_Optimized();
-add_action('in_plugin_update_message-a2-optimized-wp/a2-optimized.php', array( 'A2_Optimized','showUpgradeNotification'), 10, 2);
+add_action('in_plugin_update_message-a2-optimized-wp/a2-optimized.php', [ 'A2_Optimized','showUpgradeNotification'], 10, 2);
