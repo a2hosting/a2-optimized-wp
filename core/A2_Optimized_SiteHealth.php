c0d7cc921f375b3310d22fd390004efd5fd33a3e
Andrew Jones <ajones@a2hosting.com>
2022-10-13 07:29:16 -0400

    Fix notices in return_bytes

--- core/A2_Optimized_SiteHealth.php
+++ core/A2_Optimized_SiteHealth.php
@@ -785,8 +785,8 @@ class A2_Optimized_SiteHealth {
 	private function return_bytes($val) {
 		$val = trim($val);
 		$last = strtolower($val[strlen($val)-1]);
+		$val = substr($val, 0, -1);
 		switch($last) {
-			// The 'G' modifier is available
 			case 'g':
 				$val *= 1024;
 			case 'm':
