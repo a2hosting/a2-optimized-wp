2f0b79267e659d75cfc5c772ff80e3cf8a3d6662
Andrew Jones <ajones@a2hosting.com>
2022-10-31 16:55:45 -0400

    bumped version in plugin file

--- a2-optimized.php
+++ a2-optimized.php
@@ -2,7 +2,7 @@
 /*
     Plugin Name: A2 Optimized WP
     Plugin URI: https://wordpress.org/plugins/a2-optimized/
-    Version: 2.1.4.6.2.2
+    Version: 2.1.5
     Author: A2 Hosting
     Author URI: https://www.a2hosting.com/
     Description: A2 Optimized - WordPress Optimization Plugin
@@ -30,7 +30,7 @@ require_once 'A2_Optimized_DB_Optimizations.php';
 
 //constants
 define('A2OPT_VERSION', '2.1');
-define('A2OPT_FULL_VERSION', '2.1.4.6');
+define('A2OPT_FULL_VERSION', '2.1.5');
 define('A2OPT_MIN_PHP', '5.6');
 define('A2OPT_MIN_WP', '5.1');
 define('A2OPT_FILE', __FILE__);
