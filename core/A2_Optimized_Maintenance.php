5c86e55aa161b66a4130c53ad6e648f0e89f533b
Andrew Jones <ajones@a2hosting.com>
2023-09-19 11:56:41 -0400

    Add sync cron for xmlsitemap for litespeed

--- core/A2_Optimized_Maintenance.php
+++ core/A2_Optimized_Maintenance.php
@@ -8,12 +8,12 @@ class A2_Optimized_Maintenance {
 			return;
 		}
 
-        $this->remove_old_classes();
+		$this->remove_old_classes();
 
 		$this->hooks();
 	}
 
-    /**
+	/**
 	 * Indicate if Benchmarks is allowed to load
 	 *
 	 * @return bool
@@ -28,29 +28,50 @@ class A2_Optimized_Maintenance {
 	 * Integration hooks
 	 */
 	protected function hooks() {
-    }
+		if ($this->maybe_schedule_xmlsitemap_sync() && file_exists('/opt/a2-optimized/wordpress/class.A2_Optimized_Private_Optimizations_v3.php')) {
+			add_action('a2_sync_xmlsitemap_location', [&$this, 'sync_xmlsitemap_location']);
+			if (!wp_next_scheduled('a2_sync_xmlsitemap_location')) {
+				wp_schedule_event(time(), 'daily', 'a2_sync_xmlsitemap_location');
+			}
+		}
+	}
 
-    private function get_old_classes(){
-        $outdated_files = [
-            "A2_Optimized_CacheDisk.php",
-            "A2_Optimized_CacheEngine.php",
-            "A2_Optimized_Cache.php",
-            "A2_Optimized_CLI.php",
-            "A2_Optimized_DB_Optimizations.php",
-            "A2_Optimized_Optimizations.php",
-            "A2_Optimized_OptionsManager.php"
-        ];
+	private function maybe_schedule_xmlsitemap_sync() {
+		if (is_plugin_active('litespeed-cache/litespeed-cache.php') && is_plugin_active('google-sitemap-generator/sitemap.php')) {
+			return true;
+		}
+	}
 
-        return $outdated_files;
-    }
+	public function sync_xmlsitemap_location() {
+		$sitemap_options = get_option('sm_options');
+		$sitemap_url = $sitemap_options['sm_b_sitemap_name'];
+		$expected_sitemap = get_site_url() . '/' . $sitemap_url . '.xml';
+		$litespeed_sitemap = get_option('litespeed.conf.crawler-sitemap');
+		if (!$litespeed_sitemap || $litespeed_sitemap != $expected_sitemap) {
+			update_option('litespeed.conf.crawler-sitemap', $expected_sitemap);
+		}
+	}
 
-    private function remove_old_classes(){
-        $files = $this->get_old_classes();
-        foreach($files as $file){
-            if(file_exists(A2OPT_DIR . '/' . $file)){
-                unlink(A2OPT_DIR . '/' . $file);
-            }
-        }
-    }
+	private function get_old_classes() {
+		$outdated_files = [
+			'A2_Optimized_CacheDisk.php',
+			'A2_Optimized_CacheEngine.php',
+			'A2_Optimized_Cache.php',
+			'A2_Optimized_CLI.php',
+			'A2_Optimized_DB_Optimizations.php',
+			'A2_Optimized_Optimizations.php',
+			'A2_Optimized_OptionsManager.php'
+		];
 
+		return $outdated_files;
+	}
+
+	private function remove_old_classes() {
+		$files = $this->get_old_classes();
+		foreach ($files as $file) {
+			if (file_exists(A2OPT_DIR . '/' . $file)) {
+				unlink(A2OPT_DIR . '/' . $file);
+			}
+		}
+	}
 }
